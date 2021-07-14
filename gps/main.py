7d01c34d4678fe70d51f0133cc40187f89150174
xuli2020sj <xuli2020sj@gmail.com>
2021-07-14 11:14:47 +0800

    GPS module

---
+++ gps/main.py
@@ -0,0 +1,34 @@
+# -*- coding:utf-8 -*-
+import time
+
+import L76X
+
+# try:
+x = L76X.L76X()
+x.L76X_Set_Baudrate(9600)
+x.L76X_Send_Command(x.SET_NMEA_BAUDRATE_115200)
+time.sleep(2)
+x.L76X_Set_Baudrate(115200)
+
+x.L76X_Send_Command(x.SET_POS_FIX_400MS)
+
+# Set output message
+x.L76X_Send_Command(x.SET_NMEA_OUTPUT)
+
+x.L76X_Exit_BackupMode()
+while 1:
+    x.L76X_Gat_GNRMC()
+    if x.Status == 1:
+        print('Already positioned')
+    else:
+        print('No positioning')
+    print('Time %d: %d : %d' % (x.Time_H, x.Time_M, x.Time_S))
+
+    print('Lon = %f  Lat = %f' % (x.Lon, x.Lat))
+    x.L76X_Baidu_Coordinates(x.Lat, x.Lon)
+    print('Baidu coordinate %f ,%f' % (x.Lat_Baidu, x.Lon_Baidu))
+
+# except:
+# GPIO.cleanup()
+# print "\nProgram end"
+# exit()
