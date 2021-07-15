552fb0c846bd9eb4b4d24ea827986e1ac1582109
xuli2020sj <xuli2020sj@gmail.com>
2021-07-15 08:20:10 +0800

    camera

---
+++ cam/camera.py
@@ -0,0 +1,20 @@
+import serial
+import pynmea2
+
+
+def parseGPS(s):
+    if s.find('GGA') > -1:
+        msg = pynmea2.parse(s)
+        print("Timestamp: %s -- Lat: %s %s -- Lon: %s %s -- Altitude: %s %s" % (
+            msg.timestamp, msg.lat, msg.lat_dir, msg.lon, msg.lon_dir, msg.altitude, msg.altitude_units))
+
+
+serialPort = serial.Serial("/dev/ttyUSB0", 115200, timeout=0.5)
+while True:
+    s = serialPort.readline()
+    # print(s)
+    # print(type(s.decode()))
+    # print(s.find(b'GGA'))
+    # s = s.decode()
+    print(s)
+    # parseGPS(s)
