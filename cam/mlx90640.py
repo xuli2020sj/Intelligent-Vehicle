d4f18ddc2e853eb831ac695fac642fd2af89d65e
xuli2020sj <xuli2020sj@gmail.com>
2021-07-27 20:44:42 +0800

    update

--- cam/mlx90640.py
+++ cam/mlx90640.py
@@ -3,9 +3,6 @@ from ctypes import *
 import numpy as np
 mlx90640 = cdll.LoadLibrary('./libmlx90640.so')
 
-np.set_printoptions(threshold=np.inf)
-
-# mlx90640 will output 32*24 temperature array with chess mode
 # while True:
 #     temp = (c_float * 768)()
 #     ptemp = pointer(temp)
@@ -25,14 +22,18 @@ np.set_printoptions(threshold=np.inf)
 #     print("%.2f " % (temp[i]), end='')
 
 
+def tcam():
+    temp = (c_float * 768)()
+    ptemp = pointer(temp)
+    mlx90640.get_mlx90640_temp(ptemp)
+    my_nparray = np.frombuffer(temp, dtype=np.float32)
 
-temp = (c_float * 768)()
-ptemp = pointer(temp)
-mlx90640.get_mlx90640_temp(ptemp)
-my_nparray = np.frombuffer(temp, dtype=np.float32)
+    t = my_nparray.reshape((32, 24))
 
-t = my_nparray.reshape((32, 24))
+    # print(np.max(t))
+    # print(np.argmax(t))
+    time.sleep(1/2)
+    return np.max(t), np.argmax(t) % 32
 
-print(np.max(t))
-print(np.argmax(t))
-time.sleep(1/2)
+if __name__ == '__main__':
+    wd, co = tcam()
