fd1a1a2cb118d71f643b4b54408bedc660231574
xuli2020sj <xuli2020sj@gmail.com>
2021-07-27 12:25:36 +0800

    update

---
+++ cam/mlx90640.py
@@ -0,0 +1,15 @@
+from ctypes import *
+import numpy as np
+mlx90640 = cdll.LoadLibrary('./libmlx90640.so')
+
+
+# mlx90640 will output 32*24 temperature array with chess mode
+
+temp = (c_float * 768)()
+ptemp = pointer(temp)
+mlx90640.get_mlx90640_temp(ptemp)
+print(temp)
+# for i in range(len(temp)):
+#     if i % 32 == 0 and i != 0:
+#         print("\r\n")
+#     print("%.2f " % (temp[i]), end='')
