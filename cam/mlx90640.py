54caf9e419ce5b00636810b4a01547280091d9d7
xuli2020sj <xuli2020sj@gmail.com>
2021-07-27 14:27:07 +0800

    update

--- cam/mlx90640.py
+++ cam/mlx90640.py
@@ -8,7 +8,8 @@ mlx90640 = cdll.LoadLibrary('./libmlx90640.so')
 temp = (c_float * 768)()
 ptemp = pointer(temp)
 mlx90640.get_mlx90640_temp(ptemp)
-print(temp)
+my_nparray = np.frombuffer(temp, dtype=np.uint8)
+print(my_nparray)
 # for i in range(len(temp)):
 #     if i % 32 == 0 and i != 0:
 #         print("\r\n")
