912cb38fef971ee3087d6299267c8ea673ce561b
xuli2020sj <xuli2020sj@gmail.com>
2021-07-27 14:29:11 +0800

    update

--- cam/mlx90640.py
+++ cam/mlx90640.py
@@ -2,13 +2,14 @@ from ctypes import *
 import numpy as np
 mlx90640 = cdll.LoadLibrary('./libmlx90640.so')
 
+np.set_printoptions(threshold=np.inf)
 
 # mlx90640 will output 32*24 temperature array with chess mode
 
 temp = (c_float * 768)()
 ptemp = pointer(temp)
 mlx90640.get_mlx90640_temp(ptemp)
-my_nparray = np.frombuffer(temp, dtype=np.uint8)
+my_nparray = np.frombuffer(temp, dtype=np.float32)
 print(my_nparray)
 # for i in range(len(temp)):
 #     if i % 32 == 0 and i != 0:
