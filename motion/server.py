8a86bbb5b1cfd3fdd17b6a626d58cd2a60e19254
xuli2020sj <xuli2020sj@gmail.com>
2021-07-27 17:26:25 +0800

    update

--- motion/server.py
+++ motion/server.py
@@ -351,7 +351,7 @@ def main():
     listen_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 
     # 2.绑定端口
-    my_addr = ('192.168.126.250', 8888)
+    my_addr = ('192.168.146.107', 8888)
     listen_socket.bind(my_addr)
 
     # 3，接听状态
