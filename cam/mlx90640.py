293e789966cf38202e3ba6dc26a5ce194f383117
xuli2020sj <xuli2020sj@gmail.com>
2021-07-27 17:17:47 +0800

    update

--- cam/mlx90640.py
+++ cam/mlx90640.py
@@ -6,18 +6,33 @@ mlx90640 = cdll.LoadLibrary('./libmlx90640.so')
 np.set_printoptions(threshold=np.inf)
 
 # mlx90640 will output 32*24 temperature array with chess mode
-while True:
-    temp = (c_float * 768)()
-    ptemp = pointer(temp)
-    mlx90640.get_mlx90640_temp(ptemp)
-    my_nparray = np.frombuffer(temp, dtype=np.float32)
+# while True:
+#     temp = (c_float * 768)()
+#     ptemp = pointer(temp)
+#     mlx90640.get_mlx90640_temp(ptemp)
+#     my_nparray = np.frombuffer(temp, dtype=np.float32)
+#
+#     t = my_nparray.reshape((32, 24))
+#
+#     print(np.max(t))
+#     print(np.argmax(t))
+#     time.sleep(1/2)
 
-    t = my_nparray.reshape((32, 24))
 
-    print(np.max(t))
-    print(np.argmax(t))
-    time.sleep(1/2)
 # for i in range(len(temp)):
 #     if i % 32 == 0 and i != 0:
 #         print("\r\n")
 #     print("%.2f " % (temp[i]), end='')
+
+
+
+temp = (c_float * 768)()
+ptemp = pointer(temp)
+mlx90640.get_mlx90640_temp(ptemp)
+my_nparray = np.frombuffer(temp, dtype=np.float32)
+
+t = my_nparray.reshape((32, 24))
+
+print(np.max(t))
+print(np.argmax(t))
+time.sleep(1/2)
